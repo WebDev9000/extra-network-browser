bf49bfb0c2ff92bc9ea6ce9bdb0e0fe0d5659ed7
WebDev <6970043+WebDev9000@users.noreply.github.com>
2023-12-05 16:25:46 -0800

    Adjustments to the setup batch file.

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 @echo off
-cd %~dp0api && cmd /c npm install
-cd %~dp0app && cmd /c npm install
-cd %~dp0app && cmd /c npm run build
-cd %~dp0api/networks && cmd /c init.bat
+cd %~dp0api && call npm install --no-audit
+cd %~dp0app && call npm install --no-audit
+cd %~dp0app && call npm run build
+cd %~dp0api/networks && call init.bat
 cd %~dp0
